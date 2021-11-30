57c43d17b56ee6832699a28d22249a68f52b906f
Chris Kane <info@chriskanedev.com>
2021-11-30 11:30:17 +0000

    Add find_max.c

--- intro_programs/find_max.c
+++ intro_programs/find_max.c
@@ -2,9 +2,14 @@
 
 int main() {
     int curr;
+    int highest;
     int i;
     for (i = 0; i < 10; i++) {
         printf("Input the next number: ");
         scanf("%d", &curr);
+        if (i > highest) {
+            highest = i;
+        }
     }
+    printf("The highest number is: %d", highest);
 }
